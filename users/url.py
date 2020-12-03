e1e7dac4110700176509af9c2c3ca8ff124ffe3b
Cope <Mark_Cope1@baylor.edu>
2020-12-02 19:07:34 -0600

    Big Steps

---
+++ users/url.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+app_name = 'users'
+
+urlpatters= [
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+]
